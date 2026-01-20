e849939409a91b0b86df8dee307b2272b4062bd7
Saúl Antonio Morquecho Cela <samc920130@hotmail.com>
2026-01-19 23:25:22 -0600

    Se agrega nuevo global using

--- src/Application/GlobalUsings.cs
+++ src/Application/GlobalUsings.cs
@@ -1,4 +1,5 @@
 global using FluentValidation;
+global using MapsterMapper;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.DependencyInjection;
 global using System.Linq.Expressions;
