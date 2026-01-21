5148f413c9fd3f0da982d18f1dfa46cf9fdfc885
Saúl Antonio Morquecho Cela <samc920130@hotmail.com>
2026-01-20 22:25:10 -0600

    Se corrige propiedad de DTO

--- src/Application/UseCases/Articles/DTOs/ArticleDTO.cs
+++ src/Application/UseCases/Articles/DTOs/ArticleDTO.cs
@@ -4,5 +4,6 @@ public class ArticleDTO
 {
     public Guid Id { get; set; }
     public required string Name { get; set; }
+    public string? Description { get; set; }
     public string? Barcode { get; set; }
 }
